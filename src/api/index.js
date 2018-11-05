ef28b8575498312f68a383f90d30c68996f17aa0
HayashiMei <hayashi@kato-megumi.com>
2018-11-05 11:18:18 +0800

    Update: Some improvements for login.

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 const { stringify } = require('querystring');
+const cheerio = require('cheerio');
 const { LOGIN_URL, http } = require('../utils/http');
 const { search, Category, Tag } = require('./Search');
 const Gallery = require('./Gallery');
@@ -16,12 +17,44 @@ const login = async (username, password) => {
     ipb_login_submit: 'Login!',
   };
 
-  await http.post(LOGIN_URL, stringify(data));
-  return http.get('favorites.php');
+  try {
+    const loginResponse = await http.post(LOGIN_URL, stringify(data));
+
+    const errorMessage = parseLoginResultPage(loginResponse.data);
+
+    if (errorMessage) {
+      return Promise.reject(new TypeError(errorMessage));
+    }
+
+    return http.get('favorites.php').then(() => {
+      return {
+        cookie: http.defaults.headers.common['Cookie']
+      };
+    });
+  } catch (e) {
+    return Promise.reject(e);
+  };
+};
+
+const parseLoginResultPage = html => {
+  const $ = cheerio.load(html);
+  let errorMessage = '';
+
+  const errorNode = $('.tablepad');
+  if (errorNode.length) {
+    errorMessage = errorNode.text();
+  }
+
+  return errorMessage
+};
+
+const setCookie = cookie => {
+  http.defaults.headers.common['Cookie'] = cookie;
 };
 
 module.exports = {
   login,
+  setCookie,
   search,
   Category,
   Tag,
